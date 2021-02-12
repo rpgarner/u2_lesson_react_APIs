08db24d1336b1330dc7cdd9f1fff4966e6392219
Andre Pato <anpato1994@gmail.com>
2021-02-12 14:03:03 -0500

    finish repos

--- src/App.js
+++ src/App.js
@@ -1,9 +1,5 @@
 import './styles/App.css'
-import axios from 'axios'
 import React, { Component } from 'react'
-import { BASE_URL, axiosConfig } from './globals'
-import MovieList from './components/MovieList'
-import MovieDetails from './components/MovieDetails'
 
 export default class App extends Component {
   constructor() {
@@ -14,34 +10,7 @@ export default class App extends Component {
     }
   }
 
-  async componentDidMount() {
-    const res = await axios.get(`${BASE_URL}/discover/movie`, axiosConfig)
-    this.setState({ movies: res.data.results })
-  }
-
-  selectMovie = (movieId) => {
-    this.setState({ selectedMovie: movieId })
-  }
-
-  clearMovie = () => {
-    this.setState({ selectedMovie: null })
-  }
-
   render() {
-    return (
-      <div>
-        {this.state.selectedMovie ? (
-          <MovieDetails
-            movieId={this.state.selectedMovie}
-            goBack={this.clearMovie}
-          />
-        ) : (
-          <MovieList
-            movies={this.state.movies}
-            selectMovie={this.selectMovie}
-          />
-        )}
-      </div>
-    )
+    return <div></div>
   }
 }
